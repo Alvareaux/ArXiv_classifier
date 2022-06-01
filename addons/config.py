f9f979f231cbfa0bdf762c9cb5dd46a00b72d8eb
Anatolii <anatoly.griboedov@gmail.com>
2022-06-01 21:18:16 +0300

    Configs added

--- addons/config.py
+++ addons/config.py
@@ -106,3 +106,91 @@ class ConfigArxiv(Config):
         num_retries = int(self.config['ARXIV_API']['num_retries'])
 
         return bath_size, delay_seconds, num_retries
+
+
+class ConfigUnit:
+    def __init__(self, config_path_unit):
+        self.config = configparser.ConfigParser()
+        self.config.read(config_path_unit)
+
+        self.base_model_path = self.init_config_data()
+
+        self.threshold = self.init_config_neural()
+
+    # -----------------------------------------------------------------------------------------------------------------
+
+    def init_config_data(self):
+        base_model_path = self.config['DATA']['BaseModelPath']
+
+        return base_model_path
+
+    def init_config_neural(self):
+        threshold = float(self.config['UNIT']['Threshold'])
+
+        return threshold
+
+
+class ConfigMaster:
+    def __init__(self, config_path_master):
+        self.config = configparser.ConfigParser()
+        self.config.read(config_path_master)
+
+        self.instance_count, \
+        self.clock, \
+        self.unit, \
+        self.update_time, \
+            = self.init_config_process()
+
+        self.timeout \
+            = self.init_config_timings()
+
+        self.server, \
+        self.username, \
+        self.password, \
+        self.heartbeat, \
+        self.n_queue_process_queue, \
+        self.n_queue_process_queue_bulk, \
+        self.n_queue_response_queue, \
+        self.n_queue_response_queue_bulk, \
+        self.n_queue_master_manage, \
+        self.n_queue_master_monitor, \
+        self.n_queue_master_monitor_queue, \
+        self.n_queue_logger_base \
+            = self.init_config_server()
+
+    # -----------------------------------------------------------------------------------------------------------------
+
+    def init_config_process(self):
+        instance_count = int(self.config['MASTER']['InstanceCount'])
+
+        clock = self.config['MODULES']['Clock']
+        unit = self.config['MODULES']['Unit']
+
+        timeout = int(self.config['CLOCK']['Timeout'])
+
+        return instance_count, clock, unit, timeout
+
+    def init_config_server(self):
+        server = self.config['SERVER']['Server']
+        username = self.config['SERVER']['Username']
+        password = self.config['SERVER']['Password']
+
+        heartbeat = self.config['SERVER']['Heartbeat']
+
+        n_queue_process_queue = self.config['QUEUE']['ProcessQueue']
+        n_queue_process_queue_bulk = self.config['QUEUE']['ProcessQueueBulk']
+        n_queue_response_queue = self.config['QUEUE']['ResponseQueue']
+        n_queue_response_queue_bulk = self.config['QUEUE']['ResponseQueueBulk']
+        n_queue_master_manage = self.config['QUEUE']['MasterManage']
+        n_queue_master_monitor = self.config['QUEUE']['MasterMonitor']
+        n_queue_master_monitor_queue = self.config['QUEUE']['MasterMonitorQueue']
+        n_queue_logger_base = self.config['QUEUE']['LoggerBase']
+
+        return server, username, password, heartbeat, \
+               n_queue_process_queue, n_queue_process_queue_bulk, n_queue_response_queue, n_queue_response_queue_bulk, \
+               n_queue_master_manage, n_queue_master_monitor, n_queue_master_monitor_queue, n_queue_logger_base
+
+    def init_config_timings(self):
+        timeout = int(self.config['CLOCK']['Timeout'])
+
+        return timeout
